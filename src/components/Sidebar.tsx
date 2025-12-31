ac450075d392c6d71750a7552234df38aa2d62a2
mehedishishir0 <mehedihasanshishir.info@gmail.com>
2025-12-31 11:12:47 +0600

    v1

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import {
+  CircleParking,
   ClipboardList,
   FolderOpen,
   LayoutPanelLeft,
@@ -41,7 +42,7 @@ const navigation = [
   {
     name: "Profiles",
     href: "/dashboard/profile",
-    icon: ClipboardList,
+    icon: CircleParking,
   },
 ];
 
